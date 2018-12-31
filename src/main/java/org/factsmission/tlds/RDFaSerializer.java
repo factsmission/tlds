d2ee26841567385ebbe3de93b1d5b7fd2610890e
nleanba <noam@factsmission.com>
2018-12-31 19:03:07 +0100

    stacking multiple values per key as separate cells

--- src/main/java/org/factsmission/tlds/RDFaSerializer.java
+++ src/main/java/org/factsmission/tlds/RDFaSerializer.java
@@ -31,8 +31,10 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.apache.clerezza.commons.rdf.BlankNode;
@@ -49,16 +51,16 @@ import org.apache.clerezza.rdf.core.serializedform.UnsupportedSerializationForma
 @SupportedFormat("text/html")
 public class RDFaSerializer implements SerializingProvider {
 
-	@Override
-	public void serialize(OutputStream outputStream, Graph graph, String formatIdentifier) {
+    @Override
+    public void serialize(OutputStream outputStream, Graph graph, String formatIdentifier) {
         if (!formatIdentifier.equals("text/html")) {
             throw new UnsupportedSerializationFormatException("text/html");
         }
         PrintWriter out;
-		try {
-			out = new PrintWriter(new OutputStreamWriter(outputStream, "utf-8"));
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
+        try {
+            out = new PrintWriter(new OutputStreamWriter(outputStream, "utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         }
         Set<BlankNodeOrIRI> subjects = new HashSet<>();
         Set<RDFTerm> objects = new HashSet<>();
@@ -66,7 +68,7 @@ public class RDFaSerializer implements SerializingProvider {
             subjects.add(t.getSubject());
             RDFTerm object = t.getObject();
             objects.add(object);
-            
+
         }
         Set<BlankNodeOrIRI> exclusiveSubjects = new HashSet<>();
         exclusiveSubjects.addAll(subjects);
@@ -75,72 +77,80 @@ public class RDFaSerializer implements SerializingProvider {
         otherSubjects.addAll(subjects);
         otherSubjects.removeAll(exclusiveSubjects);
         Map<BlankNode, String> bNodeLabels = new HashMap<>();
+        out.println("<table>");
         printResources(exclusiveSubjects, graph, bNodeLabels, out);
         printResources(otherSubjects, graph, bNodeLabels, out);
-        out.flush();;
+        out.println("</table>");
+        out.flush();
     }
-    
-    
-	private void printResources(Collection<BlankNodeOrIRI> nodes, Graph graph, Map<BlankNode, String> bNodeLabels, PrintWriter out) {
+
+    private void printResources(Collection<BlankNodeOrIRI> nodes, Graph graph, Map<BlankNode, String> bNodeLabels, PrintWriter out) {
         for (BlankNodeOrIRI node : nodes) {
-            out.println("<div about='"+getLabel(node, bNodeLabels)+"' >");
-            out.println("    <h1>"+getLabel(node, bNodeLabels)+"</h1>");
+            out.println("    <tbody about='" + getLabel(node, bNodeLabels) + "' >");
+            out.println("        <tr><td colspan='2'><h1>" + getLabel(node, bNodeLabels) + "</h1></td></td>");
             printProperties(node, graph, bNodeLabels, out);
-            out.println("</div>");
+            out.println("    </tbody>");
         }
-        
-	}
 
+    }
 
-	private void printProperties(BlankNodeOrIRI node, Graph graph, Map<BlankNode, String> bNodeLabels, PrintWriter out) {
-        Iterator<Triple> matches =  graph.filter(node, null, null);
-        Map<IRI,Set<RDFTerm>> propertyValuesMap = new HashMap<>();
+    private void printProperties(BlankNodeOrIRI node, Graph graph, Map<BlankNode, String> bNodeLabels, PrintWriter out) {
+        Iterator<Triple> matches = graph.filter(node, null, null);
+        Map<IRI, Set<RDFTerm>> propertyValuesMap = new HashMap<>();
         while (matches.hasNext()) {
-            Triple t  = matches.next();
-            Set<RDFTerm> values = propertyValuesMap.containsKey(t.getPredicate()) ?
-                                    propertyValuesMap.get(t.getPredicate())
-                                    : ((Supplier<Set<RDFTerm>>)() -> {
-                                        Set<RDFTerm> n = new HashSet<>();
-                                        propertyValuesMap.put(t.getPredicate(), n);
-                                        return n;
-                                    }).get();
-            values.add(t.getObject());    
+            Triple t = matches.next();
+            Set<RDFTerm> values = propertyValuesMap.containsKey(t.getPredicate())
+                    ? propertyValuesMap.get(t.getPredicate())
+                    : ((Supplier<Set<RDFTerm>>) () -> {
+                        Set<RDFTerm> n = new HashSet<>();
+                        propertyValuesMap.put(t.getPredicate(), n);
+                        return n;
+                    }).get();
+            values.add(t.getObject());
         }
-        
-        out.println("    <table>");
-        for (final Map.Entry<IRI,Set<RDFTerm>> e : propertyValuesMap.entrySet()) {
+
+        for (final Map.Entry<IRI, Set<RDFTerm>> e : propertyValuesMap.entrySet()) {
             out.println("        <tr>");
-            out.println("            <td>");
-            out.println("                <a href='"+getLabel(e.getKey(), bNodeLabels)+"'>"+getLabel(e.getKey(), bNodeLabels)+"</a>");
+            out.println("            <td rowspan='"+e.getValue().size()+"'>");
+            out.println("                <a href='" + getLabel(e.getKey(), bNodeLabels) + "'>" + getLabel(e.getKey(), bNodeLabels) + "</a>");
             out.println("            </td>");
             out.println("            <td>");
-            out.println(String.join(", ", e.getValue().stream().map(object -> {
+            out.println(String.join("</td></tr><tr><td>", e.getValue().stream().map(object -> {
                 if (object instanceof BlankNodeOrIRI) {
-                    return "                <a property='"+getLabel(e.getKey(), bNodeLabels)+"' resource='"+getLabel((BlankNodeOrIRI) object, bNodeLabels)
-                                                    +(object instanceof IRI? "' href='"+getLabel((BlankNodeOrIRI) object, bNodeLabels):"")
-                                                    +"'>"+getLabel((BlankNodeOrIRI) object, bNodeLabels)+"</a>";
+                    return "                <a property='" + getLabel(e.getKey(), bNodeLabels) + "' resource='" + getLabel((BlankNodeOrIRI) object, bNodeLabels)
+                            + (object instanceof IRI ? "' href='" + getLabel((BlankNodeOrIRI) object, bNodeLabels) : "")
+                            + "'>" + getLabel((BlankNodeOrIRI) object, bNodeLabels) + "</a>";
                 } else {
-                    return "            <span datatype='"+((Literal)object).getDataType().getUnicodeString()+"' property='"+getLabel(e.getKey(), bNodeLabels)+"'>"+((Literal)object).getLexicalForm()+"</span>";
+                    return "                <span datatype='" + ((Literal) object).getDataType().getUnicodeString() + "' property='" + getLabel(e.getKey(), bNodeLabels) + "'>" + ((Literal) object).getLexicalForm() + "</span>";
                 }
-                
+
             }).collect(Collectors.toList())));
             out.println("            </td>");
             out.println("        </tr>");
         }
-        out.println("    </table>");
-        
-	}
 
+    }
 
-	private String getLabel(BlankNodeOrIRI node, Map<BlankNode, String> bNodeLabels) {
-		return node instanceof BlankNode? getLabel((BlankNode)node, bNodeLabels) : ((IRI)node).getUnicodeString();
+    public static Function<List<String>, String> joiningLastDelimiter(
+            String delimiter, String lastDelimiter) {
+        return list -> {
+                    int last = list.size() - 1;
+                    //if (last < 1) return String.join(delimiter, list);
+                    return String.join(lastDelimiter,
+                        String.join(delimiter, list.subList(0, last)),
+                        list.get(last));
+                };
     }
     
+    private String getLabel(BlankNodeOrIRI node, Map<BlankNode, String> bNodeLabels) {
+        return node instanceof BlankNode ? getLabel((BlankNode) node, bNodeLabels) : ((IRI) node).getUnicodeString();
+    }
+
     private String getLabel(BlankNode node, Map<BlankNode, String> bNodeLabels) {
         if (!bNodeLabels.containsKey(node)) {
-            bNodeLabels.put(node, "_:"+bNodeLabels.size());
+            bNodeLabels.put(node, "_:" + bNodeLabels.size());
         }
-		return bNodeLabels.get(node);
-	}
+        return bNodeLabels.get(node);
+    }
 
 }
